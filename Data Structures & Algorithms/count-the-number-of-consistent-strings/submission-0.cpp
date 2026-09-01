54ecbbde99b98d1179b42cc3f9e203a2edd2aa0a
Sravan-n-dev <sravannarepalem2594@gmail.com>
2026-09-01 23:57:53 +0530

    Add: count-the-number-of-consistent-strings - submission-0

---
+++ Data Structures & Algorithms/count-the-number-of-consistent-strings/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        unordered_set<char> st(allowed.begin(), allowed.end());
+
+        int ans = 0;
+
+        for (string &word : words) {
+            bool ok = true;
+
+            for (char c : word) {
+                if (!st.count(c)) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (ok) ans++;
+        }
+
+        return ans;
+    }
+};
